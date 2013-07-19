0b482ca4527365afb5c54b16fc0f7f16c85fd59f
Ubuntu <ubuntu@ip-172-31-9-199.us-west-2.compute.internal>
2013-07-19 03:25:19 +0000

    Edited paragrah one in index.html

--- web.js
+++ web.js
@@ -1,12 +1,16 @@
 var fs = require('fs');
-var express = require('express');
-var app = express.createServer(express.logger());
+var htmlfile = "index.html";
+var express = require("express");
+var app = express();
+
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync("index.html", "utf-8"));
+  var html = fs.readFileSync(htmlfile).toString();
+  response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
